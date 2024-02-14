41e53efb96bd9fe02d9facec42fe7691437f550b
keybrains <kbtplseo@gmail.com>
2024-02-14 19:56:17 +0530

    2023/02/14

--- api/app.js
+++ api/app.js
@@ -23,8 +23,8 @@ const CdnRouter = require('./routes/cdn');
 const ProjectRouter = require('./routes/Project');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
